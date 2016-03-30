eb7a8acb4712904c93ad6143d2ffcdc82620a4a7
Louisa Reese <louisa.e.reese@gmail.com>
2016-03-30 11:40:01 -0600

    we did it

--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,105 @@
-var twitterStream = angular.module('myApp', ['chart.js'])
+var twitterStream = angular.module('myApp', ['chart.js']);
 
-twitterStream.controller("mainCtrl", ['$scope', 'socket',
-function ($scope, socket) {
+twitterStream.controller("mainCtrl", ['$scope', 'socket', '$http',
+function ($scope, socket, $http) {
   //chart labels
-  $scope.labels = ["iPhone", "iPad", "Android", "Web Client", "Other"];
+  $scope.labels = ["positive", "negative", 'mixed'];
   //chart colors
-  $scope.colors = ['#6c6a6c','#000000','#7FFD1F','#EC872A', '#9527C2'];
+  $scope.colors = ['#5a8527', '#d51028', '#edd711'];
   //intial data values
-  $scope.trumpData = [0,0,0,0,0];
-  $scope.sandersData = [0,0,0,0,0];
+  $scope.trumpDataiPhone = [0,0,0];
+  $scope.trumpDataAndroid = [0,0,0];
+  $scope.sandersDataiPhone = [0,0,0];
+  $scope.sandersDataAndroid = [0,0,0];
 
   socket.on('newTweet', function (tweet) {
-    $scope.tweet = tweet.text
-    $scope.user = tweet.user.screen_name
+    console.log(tweet.created_at);
+    $scope.tweet = tweet.text;
+    $scope.user = tweet.user.screen_name;
     //parse source from payload
-    var source = tweet.source.split('>')[1].split('<')[0].split(' ')[2]
+    var source = tweet.source.split('>')[1].split('<')[0].split(' ')[2];
     //all hashtags in the tweet
     var hashtags = tweet.entities.hashtags.map(function(el){
-      return el.text.toLowerCase()
-    })
+      return el.text.toLowerCase();
+    });
 
-    //check source and increment for #trump tweets
-    if (hashtags.indexOf('trump') !== -1){
-      switch (source) {
-        case 'iPhone': $scope.trumpData[0]++
-        break;
-        case 'iPad': $scope.trumpData[1]++
-        break;
-        case 'Android': $scope.trumpData[2]++
-        break;
-        case 'Web': $scope.trumpData[3]++
-        break;
-        default: $scope.trumpData[4]++
-      }
-    }
+    var sent;
 
-    //check source and increment for #feelthebern tweets
-    else if (hashtags.indexOf('feelthebern') !== -1) {
-      switch (source) {
-        case 'iPhone': $scope.sandersData[0]++
-        break;
-        case 'iPad': $scope.sandersData[1]++
-        break;
-        case 'Android': $scope.sandersData[2]++
-        break;
-        case 'Web': $scope.sandersData[3]++
-        break;
-        default: $scope.sandersData[4]++
+    $.ajax({
+      url: '/analyze?tweet=' + tweet.text,
+      method: 'GET',
+      success: function(data) {
+        sent = data.score;
+        //check source and increment for #trump tweets
+        if ((hashtags.indexOf('trump') !== -1) && (sent > 0)){
+          switch (source) {
+            case 'iPhone': $scope.trumpDataiPhone[0]++;
+            console.log('trumpiPhone');
+            break;
+            case 'Android': $scope.trumpDataAndroid[0]++;
+            console.log('trumpAndroid');
+            break;
+          }
+        }
+        if ((hashtags.indexOf('trump') !== -1) && (sent < 0)){
+          switch (source) {
+            case 'iPhone': $scope.trumpDataiPhone[1]++;
+            console.log($scope.trumpDataiPhone);
+            console.log('trumpiPhone');
+            break;
+            case 'Android': $scope.trumpDataAndroid[1]++;
+            console.log('trumpAndroid');
+            break;
+          }
+        }
+        if ((hashtags.indexOf('trump') !== -1) && (sent === 0)) {
+          switch (source) {
+            case 'iPhone': $scope.trumpDataiPhone[2]++;
+            console.log($scope.trumpDataiPhone);
+            console.log('trumpiPhone');
+            break;
+            case 'Android': $scope.trumpDataAndroid[2]++;
+            console.log('trumpAndroid');
+            break;
+          }
+        }
+
+        //check source and increment for #feelthebern tweets
+        if ((hashtags.indexOf('feelthebern') !== -1) && (sent > 0)) {
+          switch (source) {
+            case 'iPhone': $scope.sandersDataiPhone[0]++;
+            console.log('sandersiPhone');
+            break;
+            case 'Android': $scope.sandersDataAndroid[0]++;
+            console.log('sandersAndroid');
+            break;
+          }
+        }
+        if ((hashtags.indexOf('feelthebern') !== -1) && (sent < 0)) {
+            switch (source) {
+              case 'iPhone': $scope.sandersDataiPhone[1]++;
+              console.log('sandersiPhone');
+              break;
+              case 'Android': $scope.sandersDataAndroid[1]++;
+              console.log('sandersAndroid');
+              break;
+            }
+          }
+          if ((hashtags.indexOf('feelthebern') !== -1) && (sent === 0)) {
+            switch (source) {
+              case 'iPhone': $scope.trumpDataiPhone[2]++;
+              console.log('sandersiPhone');
+              break;
+              case 'Android': $scope.trumpDataAndroid[2]++;
+              console.log('sandersAndroid');
+              break;
+            }
+          }
       }
-    }
-  });
+    });
+
+
+  }); // closes socket.on
 }
 ]);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var sentiment = require('sentiment');
 
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+router.get('/analyze', function (req, res, next) {
+  var tweet = decodeURIComponent(req.query.tweet);
+  var analyzed = sentiment(tweet);
+  console.log(analyzed.score);
+  res.status(200).send(analyzed);
 });
 
 module.exports = router;
